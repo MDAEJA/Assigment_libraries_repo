55a9f28f2ec96f892e0f938c7f42924b4b92c230
MD AEJAZ AHMED <mdaejazahmed6692@gmail.com>
2024-02-03 19:33:04 +0530

    use library chart

---
+++ script.js
@@ -0,0 +1,46 @@
+// adding config
+
+const data =  {
+    labels: ['January', 'February', 'March', 'April', 'may', 'June'],
+    datasets: [{
+      label: 'Monthly Sales',
+      data: [65, 59, 80, 81, 56, 60],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 2
+    }]
+}
+const barColor = ['red']
+
+const config = {
+    type: 'bar',
+    data: data,
+    
+    options: {
+
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+
+  const barchart = document.getElementById("mychart");
+  new Chart(barchart,config);
